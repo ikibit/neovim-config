2e7c18995441fd1fc3a88198a81244863685ca31
Barbaros <77664141+ikibit@users.noreply.github.com>
2023-01-15 22:51:37 +0300

    Update null-ls.lua

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -10,7 +10,7 @@ local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with { filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html", "json", "jsonc", "yaml", "markdown", "markdown.mdx", "graphql", "handlebars" } }, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
@@ -23,36 +23,3 @@ null_ls.setup {
   debug = true,
   sources = sources,
 }
-
--- local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
--- local event = "BufWritePre" -- or "BufWritePost"
--- local async = event == "BufWritePost"
-
--- null_ls.setup({
---   debug = true,
---   sources = sources,
---   on_attach = function(client, bufnr)
---     if client.supports_method("textDocument/formatting") then
---       vim.keymap.set("n", "<Leader>f", function()
---         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
---       end, { buffer = bufnr, desc = "[lsp] format" })
-
---       -- format on save
---       vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
---       vim.api.nvim_create_autocmd(event, {
---         buffer = bufnr,
---         group = group,
---         callback = function()
---           vim.lsp.buf.format({ bufnr = bufnr, async = async })
---         end,
---         desc = "[lsp] format on save",
---       })
---     end
-
---     if client.supports_method("textDocument/rangeFormatting") then
---       vim.keymap.set("x", "<Leader>f", function()
---         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
---       end, { buffer = bufnr, desc = "[lsp] format" })
---     end
---   end,
--- })
