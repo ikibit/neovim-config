9f690209e6b9876850f196f71a58568c8098f3c3
Barbaros <77664141+ikibit@users.noreply.github.com>
2023-01-26 17:40:52 +0300

    Update init.lua

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,31 +1,15 @@
-local overrides = require "custom.plugins.overrides"
+-- local overrides = require "custom.plugins.overrides"
 
 return {
-
-  -- ["goolord/alpha-nvim"] = { disable = false } -- enables dashboard
-
   -- Override plugin definition options
   ["neovim/nvim-lspconfig"] = {
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.plugins.lspconfig"
-    end,
+        end,
   },
 
-  -- overrde plugin configs
-  ["nvim-treesitter/nvim-treesitter"] = {
-    override_options = overrides.treesitter,
-  },
-
-  ["williamboman/mason.nvim"] = {
-    override_options = overrides.mason,
-  },
-
-  ["kyazdani42/nvim-tree.lua"] = {
-    override_options = overrides.nvimtree,
-  },
-
-  -- Install a plugin
+  -- Better escape
   ["max397574/better-escape.nvim"] = {
     event = "InsertEnter",
     config = function()
@@ -33,14 +17,11 @@ return {
     end,
   },
 
-  -- code formatting, linting etc
+  -- Code formatting, linting etc
   ["jose-elias-alvarez/null-ls.nvim"] = {
     after = "nvim-lspconfig",
     config = function()
       require "custom.plugins.null-ls"
     end,
   },
-
-  -- remove plugin
-  -- ["hrsh7th/cmp-path"] = false,
 }
