bdd405857bcc5895624aacec634d19b649675276
Barbaros <77664141+ikibit@users.noreply.github.com>
2023-01-26 17:19:44 +0300

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -3,9 +3,8 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>tc"] = { "<cmd> NvimTreeCollapse <CR>", "Collapse nvimtree" },
   },
 }
-
--- more keybinds!
 
 return M
