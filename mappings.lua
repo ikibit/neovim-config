3e9ede69ceed8c63ea6fc1d75d9df5520a8d12bf
Barbaros <77664141+ikibit@users.noreply.github.com>
2023-02-03 11:05:26 +0300

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -4,9 +4,12 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>tc"] = { "<cmd> NvimTreeCollapse <CR>", "Collapse nvimtree" },
-    ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Collapse nvimtree" },
-    ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Collapse nvimtree" },
+    ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Telescope buffers" },
+    ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Telescope help_tags" },
+    ["<leader>cs"] = { "<cmd> Telescope commands <CR>", "Telescope commands" },
+    ["<leader>ch"] = { "<cmd> Telescope command_history <CR>", "Telescope command_history" },
   },
 }
 
 return M
+
